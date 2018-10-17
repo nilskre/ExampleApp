26fe6e07bdddbcf2dfe9b223dd9d6c00314498d8
Nils Krehl <nils.krehl@lherk.de>
2018-10-17 20:59:40 +0200

    read input of EditText while creating Activity

--- app/src/main/java/com/example/android/exampleapp/MainActivity.java
+++ app/src/main/java/com/example/android/exampleapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.exampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView myTextView = findViewById(R.id.textView);
+        TextView myTextView = (TextView)findViewById(R.id.textView);
         myTextView.setText("Test");
+
+        EditText myEditText = (EditText)findViewById(R.id.editText);
+        String input = myEditText.getText().toString();
     }
 }
